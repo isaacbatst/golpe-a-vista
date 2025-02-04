469941aa3026a42a25f3b2500ade24cf8b6979c2
Isaac Batista <isaacbatst@gmail.com>
2025-02-04 16:44:44 -0300

    ✨ Adiciona deck

---
+++ src/domain/deck.test.ts
@@ -0,0 +1,21 @@
+import { expect, it } from "vitest";
+import { Deck } from "./deck";
+
+it("deve criar um deck", () => {
+  const [error, deck] = Deck.create([1,2,3,4])
+  expect(error).toBeUndefined();
+  expect(deck).toBeDefined();
+})
+
+it("não deve criar um deck vaio", () => {
+  const [error, deck] = Deck.create([])
+  expect(error).toBe("O baralho não pode ser vazio");
+  expect(deck).toBeUndefined();
+})
+
+it("deve comprar uma carta do deck", () => {
+  const [error, deck] = Deck.create([1,2,3,4])
+  expect(error).toBeUndefined();
+  const card = deck!.draw();
+  expect(card).toBeOneOf([1,2,3,4]);
+})
---
+++ src/domain/deck.ts
@@ -0,0 +1,31 @@
+import { Either, left, right } from "./either";
+
+export class Deck<T> {
+  private _cards: T[];
+  private _allCards: T[];
+
+  private constructor(cards: T[]) {
+    this._cards = cards;
+    this._allCards = cards;
+    this.shuffle();
+  }
+
+  static create<T>(cards: T[]): Either<string, Deck<T>> {
+    if (cards.length === 0) {
+      return left("O baralho não pode ser vazio");
+    }
+    return right(new Deck(cards));
+  }
+
+  shuffle(): void {
+    this._cards = this._cards.sort(() => Math.random() - 0.5);
+  }
+
+  draw(): T | undefined {
+    return this._cards.pop();
+  }
+
+  get size(): number {
+    return this._cards.length;
+  }
+}
