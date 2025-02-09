2684f13e7a6fd7e27b3419ee1e2df203edf7218f
Isaac Batista <isaacbatst@gmail.com>
2025-02-09 09:30:17 -0300

    ✨ Adiciona radicalização no Player

--- src/domain/player.ts
+++ src/domain/player.ts
@@ -1,7 +1,33 @@
+import { Either, left, right } from "./either";
 import { Role } from "./role";
 
 export class Player {
-  constructor(private readonly _name: string, private readonly _role: Role, public impeached = false) {}
+  constructor(
+    private readonly _name: string,
+    private readonly _role: Role,
+    public impeached = false,
+    private _radicalized = false
+  ) {}
+
+  radicalize(): Either<string, boolean> {
+    if (this.impeached) {
+      return left("Jogador cassado não pode ser radicalizado.");
+    }
+
+    if (this._role === Role.RADICAL) {
+      return left("Jogador não pode ser radicalizado pois já é radical.");
+    }
+    if (this._radicalized) {
+      return left("Jogador já foi radicalizado.");
+    }
+
+    if (this._role === Role.CONSERVADOR) {
+      return right(false);
+    }
+    
+    this._radicalized = true;
+    return right(true);
+  }
 
   get name() {
     return this._name;
@@ -9,5 +35,9 @@ export class Player {
 
   get role() {
     return this._role;
+  }
+
+  get radicalized() {
+    return this._radicalized;
   }
 }
