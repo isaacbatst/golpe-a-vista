c8ee36970be3ab98848efa968716aa45ad7e5472
Isaac Batista <isaacbatst@gmail.com>
2025-02-28 17:32:33 -0300

    🐛 Ajusta overlay com mobile

--- app/src/hooks/useOverlay.ts
+++ app/src/hooks/useOverlay.ts
@@ -2,26 +2,23 @@ import { useState } from "react";
 
 export const useOverlay = (initialValue = false, isOverlayFixed = false) => {
   const [showingOverlay, setShowingOverlay] = useState(initialValue);
-  const [isTouchDevice, setIsTouchDevice] = useState(false);
+  const isTouchDevice = () => {
+    if (typeof window === "undefined") return false;
+    return "ontouchstart" in window || navigator.maxTouchPoints > 0;
+  }
 
   const handleMouseEnter = (isOverlayFixed: boolean) => {
-    if (isTouchDevice || isOverlayFixed) return;
+    if (isTouchDevice() || isOverlayFixed) return;
     setShowingOverlay(true);
   };
 
   const handleMouseLeave = (isOverlayFixed: boolean) => {
-    if (isTouchDevice || isOverlayFixed) return;
+    if (isTouchDevice() || isOverlayFixed) return;
     setShowingOverlay(false);
-  };
-
-  const handleClick = (isOverlayFixed: boolean) => {
-    if (isTouchDevice || isOverlayFixed) return;
-    setShowingOverlay((prev) => !prev);
   };
 
   const handleTouchStart = (isOverlayFixed: boolean) => {
     if (isOverlayFixed) return;
-    setIsTouchDevice(true);
     setShowingOverlay((prev) => !prev);
   };
 
@@ -30,7 +27,6 @@ export const useOverlay = (initialValue = false, isOverlayFixed = false) => {
     containerProps: {
       onMouseEnter: () => handleMouseEnter(isOverlayFixed),
       onMouseLeave: () => handleMouseLeave(isOverlayFixed),
-      onClick: () => handleClick(isOverlayFixed),
       onTouchStart: () => handleTouchStart(isOverlayFixed),
     },
   };
