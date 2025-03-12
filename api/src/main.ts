c6424e8be350476efb696fde3f7db89fea04b417
Isaac Batista <isaacbatst@gmail.com>
2025-03-12 11:03:46 -0300

    🐛 Adiciona config para confiar no proxy

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: process.env.CORS_ORIGIN ?? 'http://localhost:3000',
     credentials: true,
   });
+  app.set('trust proxy', 1);
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap().catch((err) => console.error(err));
